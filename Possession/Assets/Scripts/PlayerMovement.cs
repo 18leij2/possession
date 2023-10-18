b096145f21b32bed261d52e7525b545185cc7a5c
18leij2 <jasonleiqi@gmail.com>
2023-10-18 16:41:27 -0400

    ghost movement
    will probably "refine" it later

---
+++ Possession/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            Debug.Log("left");
+            transform.position = new Vector2(transform.position.x - moveSpeed, transform.position.y);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            Debug.Log("right");
+            transform.position = new Vector2(transform.position.x + moveSpeed, transform.position.y);
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            Debug.Log("up");
+            transform.position = new Vector2(transform.position.x, transform.position.y + moveSpeed);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            Debug.Log("down");
+            transform.position = new Vector2(transform.position.x, transform.position.y - moveSpeed);
+        }
+    }
+}
