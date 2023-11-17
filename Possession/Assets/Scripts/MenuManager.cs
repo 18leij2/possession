d7479132b8eb56c5cbbb0842a91c263e5818b67a
Luke Walter <59621677+LukeWalter@users.noreply.github.com>
2023-11-17 16:27:56 -0500

    Added text menu options

---
+++ Possession/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour {
+
+    [SerializeField] GameObject[] menuItems;
+
+    // Start is called before the first frame update
+    void Start() {
+        
+    } // Start
+
+    // Update is called once per frame
+    void Update() {
+        
+    } // Update
+}
