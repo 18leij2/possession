2ca18a1ce839683e90a2c179412b1b1415976067
Mulloy, Sean Austin <smulloy3@gatech.edu>
2023-12-06 22:13:14 -0500

    UI PERSISTS

--- Possession/Assets/Scripts/PlayerMovement.cs
+++ Possession/Assets/Scripts/PlayerMovement.cs
@@ -24,7 +24,7 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        SceneManager.LoadScene("UI", LoadSceneMode.Additive);
     }
 
     // Update is called once per frame
